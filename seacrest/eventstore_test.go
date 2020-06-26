9479ebeaa8f34180aa31aab04c34ee34f2a377fd
Alex Gemmell <agemmell@gmail.com>
2020-06-26 00:29:52 -0700

    Test eventstore PersistEvents()

--- seacrest/eventstore_test.go
+++ seacrest/eventstore_test.go
@@ -10,3 +10,24 @@ func Test_NewEventStore_GetAllEvents(t *testing.T) {
 	eventStore := NewEventStore()
 	assert.Len(t, eventStore.GetAllEvents(), 0)
 }
+
+func Test_EventStore_PersistEvents(t *testing.T) {
+	event1 := Message{
+		messageID:   "test-id-1",
+		messageType: "test-type-a",
+	}
+
+	event2 := Message{
+		messageID:   "test-id-2",
+		messageType: "test-type-b",
+	}
+
+	event3 := Message{
+		messageID:   "test-id-3",
+		messageType: "test-type-c",
+	}
+
+	eventStore := NewEventStore()
+	eventStore.PersistEvents(event1, event2, event3)
+	assert.Len(t, eventStore.events, 3)
+}
